05c1a158d2c8db6059fe828c501a31928ba1f372
한예원 <yewon@yewon-2.local>
2025-08-01 02:09:50 +0900

    yewon 4 commit

--- dataset.js
+++ dataset.js
@@ -12,11 +12,11 @@ let users = [
 ];
 
 let currentMovies = [
-{title:'좀비딸',age:12,shortInfo:"2025.07.30 개봉1시간 54분코미디, 드라마",longInfo:"",poster:"movies/movie1.png"},
-{title:'이사',age:0,shortInfo:"2025.07.23 개봉2시간 5분드라마",longInfo:"",poster:"movies/movie2.png"},
-{title:'델마와 루이스',age:19,shortInfo:"2025.07.10 개봉2시간 9분드라마",longInfo:"",poster:"movies/movie3.png"},
-{title:'인생은 아름다워',age:0,shortInfo:"2016.04.13 개봉1시간 56분코미디, 드라마, 전쟁",longInfo:"",poster:"movies/movie4.png"},
-{title:'발레리나',age:19,shortInfo:"2025.08.06 개봉2시간 5분액션",longInfo:"",poster:"movies/movie5.png"}
+{id:1,title:'좀비딸',age:"icons/age12.png",shortInfo:"2025.07.30 개봉1시간 54분코미디, 드라마",longInfo:"",poster:"movies/movie1.png"},
+{id:2,title:'이사',age:"icons/age0.png",shortInfo:"2025.07.23 개봉2시간 5분드라마",longInfo:"",poster:"movies/movie2.png"},
+{id:3,title:'델마와 루이스',age:"icons/age19.png",shortInfo:"2025.07.10 개봉2시간 9분드라마",longInfo:"",poster:"movies/movie3.png"},
+{id:4,title:'인생은 아름다워',age:"icons/age0.png",shortInfo:"2016.04.13 개봉1시간 56분코미디, 드라마, 전쟁",longInfo:"",poster:"movies/movie4.png"},
+{id:5,title:'발레리나',age:"icons/age19.png",shortInfo:"2025.08.06 개봉2시간 5분액션",longInfo:"",poster:"movies/movie5.png"}
 ];
 
 localStorage.setItem("users",JSON.stringify(users));
