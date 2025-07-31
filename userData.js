2b02ba939155b85e42fbcfa75bed8871fda8432b
yewon <1111@icloud.com>
2025-07-31 13:13:11 +0900

    first

---
+++ userData.js
@@ -0,0 +1,14 @@
+let selectedMovie = "";
+let selectedMem = "";
+let selectedSeat = "";
+let status = false;
+
+let users = [
+  { id:"yewon", pw:"gksdPdnjs", name:"한예원", coupon:1, tickets:[] },
+  { id:"gahyeon", pw:"rlarkgus", name:"김가현", coupon:2, tickets:[] },
+  { id:"yeonseo", pw:"thsdustj", name:"손연서", coupon:2, tickets:[] },
+  { id:"taeho", pw:"gkxogh", name:"하태호", coupon:1, tickets:[] },
+  { id:"jinwoo", pw:"tjdwlsdn", name:"성진우", coupon:1, tickets:[] }
+];
+
+let loginMember = "";
